bf28607bebffffe2ba474d83d24a986fcd329a81
Andrew Sonin <me@andrewsonin.ru>
2023-10-16 12:42:40 +0300

    docs(utils/private-index): add explanation

--- src/utils/private_index.rs
+++ src/utils/private_index.rs
@@ -1,5 +1,7 @@
 use std::hash::Hash;
 
+/// Necessary to protect the internal `usize`, which, in the absence of this wrapper,
+/// would be public in the module calling [`register_blazemap_id`](crate::register_blazemap_id).
 #[derive(Clone, Copy, Eq, PartialEq, Hash)]
 #[repr(transparent)]
 #[doc(hidden)]
